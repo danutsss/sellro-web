3ad2116ade22fde095dccdd625bf9665b152cb4a
danut <matei1337@icloud.com>
2023-07-25 16:02:07 +0300

    registering service worker in workbox window

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require("./bootstrap");
+import { Workbox } from "worbox-window";
+
+if ("serviceWorker" in navigator) {
+    const wb = new Workbox("/service-worker.js");
+
+    wb.register();
+}
