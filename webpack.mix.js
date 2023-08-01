37498d6f60eb24de09a40aa112c5eae5657da3f3
danut <matei1337@icloud.com>
2023-08-01 10:02:06 +0300

    removed pwa laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
-const mix = require("laravel-mix");
-const path = require("path");
-require("laravel-mix-workbox");
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,176 +11,126 @@ require("laravel-mix-workbox");
  |
  */
 
-mix.js("resources/js/app.js", "public/js");
+mix.js('resources/js/app.js', 'public/js');
 
-mix.sass("resources/sass/app.scss", "public/css");
-mix.sass("resources/sass/app.rtl.scss", "public/css");
-mix.sass("resources/sass/admin.scss", "public/css");
+mix.sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.rtl.scss', 'public/css');
+mix.sass('resources/sass/admin.scss', 'public/css');
 
 mix.options({ processCssUrls: false });
 
-mix.generateSW({
-    // Set the path to the service-worker.js file.
-    swDest: path.join(`${__dirname}/public`, "service-worker.js"),
-
-    // Do not precache images
-    exclude: [
-        /\.(?:png|jpg|jpeg|svg)$/,
-        // Ignore the mix.js that's being generated
-        "mix.js",
-    ],
-
-    // Define runtime caching rules.
-    runtimeCaching: [
-        {
-            // Match any request that ends with .png, .jpg, .jpeg or .svg.
-            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-            // Apply a cache-first strategy.
-            handler: "CacheFirst",
-
-            options: {
-                // Use a custom cache name.
-                cacheName: "images",
-            },
-        },
-    ],
-
-    clientsClaim: true,
-
-    skipWaiting: true,
-});
-
-mix.injectManifest({
-    swSrc: path.join(`${__dirname}/resources/js`, "service-worker.js"),
-});
 /* Combine CSS */
-mix.combine(
-    [
-        "public/css/app.css",
-        "public/assets/bootstrap/css/bootstrap.min.css",
-        "public/assets/plugins/select2/css/select2.min.css",
-        "public/assets/css/style.css",
-        "public/assets/css/style-main.css",
-        "public/assets/css/skins/default.css",
-        "public/assets/plugins/owlcarousel/assets/owl.carousel.min.css",
-        "public/assets/plugins/owlcarousel/assets/owl.theme.default.min.css",
-        "public/assets/css/flags/flags.min.css",
-        "public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css",
-        "public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css",
-        "public/assets/plugins/busy-load/0.1.2/app.min.css",
-    ],
-    "public/css/app.css"
-);
+mix.combine([
+	'public/css/app.css',
+	'public/assets/bootstrap/css/bootstrap.min.css',
+	'public/assets/plugins/select2/css/select2.min.css',
+	'public/assets/css/style.css',
+	'public/assets/css/style-main.css',
+	'public/assets/css/skins/default.css',
+	'public/assets/plugins/owlcarousel/assets/owl.carousel.min.css',
+	'public/assets/plugins/owlcarousel/assets/owl.theme.default.min.css',
+	'public/assets/css/flags/flags.min.css',
+	'public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css',
+	'public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css',
+	'public/assets/plugins/busy-load/0.1.2/app.min.css',
+], 'public/css/app.css');
 
 /* Combine RTL CSS */
-mix.combine(
-    [
-        "public/css/app.rtl.css",
-        "public/assets/bootstrap/css/bootstrap.rtl.css",
-        "public/assets/plugins/select2/css/select2.min.css",
-        "public/assets/css/rtl/style.css",
-        "public/assets/css/rtl/style-main.css",
-        "public/assets/css/skins/default.css",
-        "public/assets/plugins/owlcarousel/assets/owl.carousel.min.css",
-        "public/assets/plugins/owlcarousel/assets/owl.theme.default.min.css",
-        "public/assets/css/flags/flags.min.css",
-        "public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css",
-        "public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css",
-        "public/assets/plugins/busy-load/0.1.2/app.min.css",
-    ],
-    "public/css/app.rtl.css"
-);
+mix.combine([
+	'public/css/app.rtl.css',
+	'public/assets/bootstrap/css/bootstrap.rtl.css',
+	'public/assets/plugins/select2/css/select2.min.css',
+	'public/assets/css/rtl/style.css',
+	'public/assets/css/rtl/style-main.css',
+	'public/assets/css/skins/default.css',
+	'public/assets/plugins/owlcarousel/assets/owl.carousel.min.css',
+	'public/assets/plugins/owlcarousel/assets/owl.theme.default.min.css',
+	'public/assets/css/flags/flags.min.css',
+	'public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css',
+	'public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css',
+	'public/assets/plugins/busy-load/0.1.2/app.min.css',
+], 'public/css/app.rtl.css');
 
 /* Combine JS */
-mix.combine(
-    [
-        "public/assets/plugins/jquery/1.11.1/jquery.min.js",
-        "public/assets/plugins/jqueryui/1.9.2/jquery-ui.min.js",
-        /* 'public/assets/plugins/popper.js/1.14.7/popper.min.js', */
-        /* 'public/assets/bootstrap/js/bootstrap.min.js', */
-        /* Bundled JS files (bootstrap.bundle.js and minified bootstrap.bundle.min.js) include Popper, but not jQuery. */
-        "public/assets/bootstrap/js/bootstrap.bundle.min.js",
-        "public/js/app.js",
-        "public/assets/plugins/jquery.fs.scroller/jquery.fs.scroller.min.js",
-        "public/assets/plugins/select2/js/select2.full.min.js",
-        "public/assets/plugins/SocialShare/SocialShare.min.js",
-        "public/assets/js/jquery.parallax-1.1.js",
-        "public/assets/js/hideMaxListItem-min.js",
-        "public/assets/plugins/jquery-nice-select/js/jquery.nice-select.min.js",
-        "public/assets/plugins/jquery.nicescroll/dist/jquery.nicescroll.min.js",
-        "public/assets/plugins/owlcarousel/owl.carousel.js",
-        "public/assets/plugins/pnotify/5.2.0/dist/PNotify.js",
-        "public/assets/plugins/sweetalert2/11.1.10/sweetalert2.all.min.js",
-        "public/assets/plugins/autocomplete/jquery.mockjax.js",
-        "public/assets/plugins/autocomplete/jquery.autocomplete.min.js",
-        "public/assets/plugins/bootstrap-waitingfor/bootstrap-waitingfor.min.js",
-        "public/assets/plugins/counter-up/waypoints.min.js",
-        "public/assets/plugins/counter-up/jquery.counterup.min.js",
-        "public/assets/plugins/intl-tel-input/17.0.18/js/intlTelInput.min.js",
-        "public/assets/plugins/busy-load/0.1.2/app.min.js",
-
-        "public/assets/js/ajaxSetup.js",
-        "public/assets/js/global.js",
-        "public/assets/js/script.js",
-        "public/assets/js/form-validation.js",
-        "public/assets/js/app/autocomplete.cities.js",
-        "public/assets/js/app/auth-fields.js",
-        "public/assets/js/app/show.phone.js",
-        "public/assets/js/app/make.favorite.js",
-    ],
-    "public/js/app.js"
-);
+mix.combine([
+	'public/assets/plugins/jquery/1.11.1/jquery.min.js',
+	'public/assets/plugins/jqueryui/1.9.2/jquery-ui.min.js',
+	/* 'public/assets/plugins/popper.js/1.14.7/popper.min.js', */
+	/* 'public/assets/bootstrap/js/bootstrap.min.js', */
+	/* Bundled JS files (bootstrap.bundle.js and minified bootstrap.bundle.min.js) include Popper, but not jQuery. */
+	'public/assets/bootstrap/js/bootstrap.bundle.min.js',
+	'public/js/app.js',
+	'public/assets/plugins/jquery.fs.scroller/jquery.fs.scroller.min.js',
+	'public/assets/plugins/select2/js/select2.full.min.js',
+	'public/assets/plugins/SocialShare/SocialShare.min.js',
+	'public/assets/js/jquery.parallax-1.1.js',
+	'public/assets/js/hideMaxListItem-min.js',
+	'public/assets/plugins/jquery-nice-select/js/jquery.nice-select.min.js',
+	'public/assets/plugins/jquery.nicescroll/dist/jquery.nicescroll.min.js',
+	'public/assets/plugins/owlcarousel/owl.carousel.js',
+	'public/assets/plugins/pnotify/5.2.0/dist/PNotify.js',
+	'public/assets/plugins/sweetalert2/11.1.10/sweetalert2.all.min.js',
+	'public/assets/plugins/autocomplete/jquery.mockjax.js',
+	'public/assets/plugins/autocomplete/jquery.autocomplete.min.js',
+	'public/assets/plugins/bootstrap-waitingfor/bootstrap-waitingfor.min.js',
+	'public/assets/plugins/counter-up/waypoints.min.js',
+	'public/assets/plugins/counter-up/jquery.counterup.min.js',
+	'public/assets/plugins/intl-tel-input/17.0.18/js/intlTelInput.min.js',
+	'public/assets/plugins/busy-load/0.1.2/app.min.js',
+	
+	'public/assets/js/ajaxSetup.js',
+	'public/assets/js/global.js',
+	'public/assets/js/script.js',
+	'public/assets/js/form-validation.js',
+	'public/assets/js/app/autocomplete.cities.js',
+	'public/assets/js/app/auth-fields.js',
+	'public/assets/js/app/show.phone.js',
+	'public/assets/js/app/make.favorite.js'
+], 'public/js/app.js');
 
 /* ADMIN PANEL */
 
 /* Combine CSS for Admin Panel */
-mix.combine(
-    [
-        "public/css/admin.css",
-        "public/assets/plugins/pace/1.0.2/pace.min.css",
-        "public/assets/plugins/pnotify/5.2.0/dist/PNotify.css",
-        "public/assets/plugins/pnotify/5.2.0/modules/bootstrap4/PNotifyBootstrap4.css",
-        "public/assets/plugins/pnotify/5.2.0/modules/confirm/PNotifyConfirm.css",
-        "public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css",
-        "public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css",
-        "public/assets/plugins/busy-load/0.1.2/app.min.css",
-
-        "public/assets/admin/css/style.css",
-        "public/assets/admin/css/style-main.css",
-    ],
-    "public/css/admin.css"
-);
+mix.combine([
+	'public/css/admin.css',
+	'public/assets/plugins/pace/1.0.2/pace.min.css',
+	'public/assets/plugins/pnotify/5.2.0/dist/PNotify.css',
+	'public/assets/plugins/pnotify/5.2.0/modules/bootstrap4/PNotifyBootstrap4.css',
+	'public/assets/plugins/pnotify/5.2.0/modules/confirm/PNotifyConfirm.css',
+	'public/assets/plugins/sweetalert2/11.1.10/sweetalert2.min.css',
+	'public/assets/plugins/intl-tel-input/17.0.18/css/intlTelInput.css',
+	'public/assets/plugins/busy-load/0.1.2/app.min.css',
+	
+	'public/assets/admin/css/style.css',
+	'public/assets/admin/css/style-main.css'
+], 'public/css/admin.css');
 
 /* Combine JS for Admin Panel */
-mix.combine(
-    [
-        "public/assets/plugins/jquery/3.3.1/jquery.min.js",
-        "public/assets/plugins/popper.js/2.9.3/popper.min.js",
-        "public/assets/bootstrap/js/bootstrap.min.js",
-        "public/assets/admin/js/app.js",
-        "public/assets/plugins/perfect-scrollbar/0.7.1/perfect-scrollbar.jquery.min.js",
-        "public/assets/plugins/sparkline/sparkline.js",
-        "public/assets/admin/js/waves.js",
-        "public/assets/admin/js/sidebarmenu.js",
-        "public/assets/admin/js/feather.min.js",
-        "public/assets/admin/js/custom.js",
-        "public/assets/plugins/pnotify/5.2.0/dist/PNotify.js",
-        "public/assets/plugins/pnotify/5.2.0/modules/bootstrap4/PNotifyBootstrap4.js",
-        "public/assets/plugins/pnotify/5.2.0/modules/font-awesome5/PNotifyFontAwesome5.js",
-        "public/assets/plugins/pnotify/5.2.0/modules/font-awesome5-fix/PNotifyFontAwesome5Fix.js",
-        "public/assets/plugins/pnotify/5.2.0/modules/confirm/PNotifyConfirm.js",
-        "public/assets/plugins/sweetalert2/11.1.10/sweetalert2.all.min.js",
-        "public/assets/plugins/pace/1.0.2/pace.min.js",
-        "public/assets/plugins/intl-tel-input/17.0.18/js/intlTelInput.min.js",
-        "public/assets/plugins/busy-load/0.1.2/app.min.js",
-
-        "public/assets/js/ajaxSetup.js",
-        "public/assets/js/global.js",
-        "public/assets/js/app/auth-fields.js",
-    ],
-    "public/js/admin.js"
-);
+mix.combine([
+	'public/assets/plugins/jquery/3.3.1/jquery.min.js',
+	'public/assets/plugins/popper.js/2.9.3/popper.min.js',
+	'public/assets/bootstrap/js/bootstrap.min.js',
+	'public/assets/admin/js/app.js',
+	'public/assets/plugins/perfect-scrollbar/0.7.1/perfect-scrollbar.jquery.min.js',
+	'public/assets/plugins/sparkline/sparkline.js',
+	'public/assets/admin/js/waves.js',
+	'public/assets/admin/js/sidebarmenu.js',
+	'public/assets/admin/js/feather.min.js',
+	'public/assets/admin/js/custom.js',
+	'public/assets/plugins/pnotify/5.2.0/dist/PNotify.js',
+	'public/assets/plugins/pnotify/5.2.0/modules/bootstrap4/PNotifyBootstrap4.js',
+	'public/assets/plugins/pnotify/5.2.0/modules/font-awesome5/PNotifyFontAwesome5.js',
+	'public/assets/plugins/pnotify/5.2.0/modules/font-awesome5-fix/PNotifyFontAwesome5Fix.js',
+	'public/assets/plugins/pnotify/5.2.0/modules/confirm/PNotifyConfirm.js',
+	'public/assets/plugins/sweetalert2/11.1.10/sweetalert2.all.min.js',
+	'public/assets/plugins/pace/1.0.2/pace.min.js',
+	'public/assets/plugins/intl-tel-input/17.0.18/js/intlTelInput.min.js',
+	'public/assets/plugins/busy-load/0.1.2/app.min.js',
+	
+	'public/assets/js/ajaxSetup.js',
+	'public/assets/js/global.js',
+	'public/assets/js/app/auth-fields.js',
+], 'public/js/admin.js');
 
 /* Disable Compilation Notification */
 /* mix.disableNotifications(); */
